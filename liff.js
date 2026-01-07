ae0dd4a682487bc92f0f27d2cbde0a4a56947066
ryotakakuwa0129-sudo <ryotakakuwa0129@gmail.com>
2026-01-07 11:19:29 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -1,5 +1,5 @@
-const LIFF_ID = "YOUR_LIFF_ID";
-const GAS_URL = "YOUR_GAS_WEBAPP_URL";
+const LIFF_ID = "2008725002-jHJsEKRx";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbz910FJbvEKdMV-8dEGncfx2YocZHmNVeuyZHRA26c6SmqEaBEgPzwURfl1fQonvpTbpQ/exec";
 
 async function initLiff() {
   await liff.init({ liffId: LIFF_ID });
