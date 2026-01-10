7bfbb79d680b750ecc3d66203676c858a1cb9d70
ryotakakuwa0129-sudo <ryotakakuwa0129@gmail.com>
2026-01-10 13:26:42 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -2,14 +2,10 @@
 // 設定
 // =====================
 const LIFF_ID = "2008725002-jHJsEKRx";
-const GAS_URL = "https://script.google.com/macros/s/AKfycby0tjXYVUWyPRwqs7r7PwJrrslfTCdZIeQmFwwT1JUfMF9N4a6XwXtgvMz-JDIzIt_mxQ/exec";
-// =====================
-// =====================
+const GAS_URL = "https://script.google.com/macros/s/AKfycby0tjXYVUWyPRwqs7r7PwJrrslfTCdZIeQmFwwT1JUfMF9N4a6XwXtgvMz-JDIzIt_mxQ/exec";;
+
 let userId = null;
 
-// =====================
-// LIFF共通初期化
-// =====================
 async function initLIFF(page) {
   await liff.init({ liffId: LIFF_ID });
 
@@ -20,16 +16,12 @@ async function initLIFF(page) {
 
   userId = liff.getDecodedIDToken().sub;
 
-  // ページ別処理
   if (page === "add") initAdd();
   if (page === "done") initDone();
   if (page === "register") initRegister();
 }
 
-// =====================
-// GAS API
-// =====================
-async function api(action, data={}) {
+async function api(action, data = {}) {
   const res = await fetch(GAS_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
