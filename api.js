4ded878829fb037d5ee3eab5227cedcbe237114f
ryotakakuwa0129-sudo <ryotakakuwa0129@gmail.com>
2026-01-10 21:31:37 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,15 +1,11 @@
 const GAS_URL =
 "https://script.google.com/macros/s/AKfycby0tjXYVUWyPRwqs7r7PwJrrslfTCdZIeQmFwwT1JUfMF9N4a6XwXtgvMz-JDIzIt_mxQ/exec";
 
-function api(data) {
-  return fetch(GAS_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  }).then(r => r.json());
+async function api(data){
+  const r = await fetch(GAS_URL,{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify(data)
+  });
+  return r.json();
 }
-
-    await api("doneHomework", { doneList: list });
-    liff.closeWindow();
-  }
-});
