9941787921bc342b18f3d84c577e1335418defd2
ryotakakuwa0129-sudo <ryotakakuwa0129@gmail.com>
2026-01-11 19:17:14 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,22 +1,14 @@
 const GAS_URL = "https://script.google.com/macros/s/AKfycby0tjXYVUWyPRwqs7r7PwJrrslfTCdZIeQmFwwT1JUfMF9N4a6XwXtgvMz-JDIzIt_mxQ/exec";
 
-const API = {
-  post(action, data) {
-    return fetch(GAS_URL, {
-      method: "POST",
-      headers: {"Content-Type":"application/json"},
-      body: JSON.stringify({ action, ...data })
-    }).then(r=>r.json());
-  },
-  register(userId){ return this.post("register",{userId}); },
-  addHomework(subject,text,date){
-    return this.post("addHomework",{subject,text,date});
-  },
-  getUndone(userId){
-    return this.post("getUndoneHomework",{userId});
-  },
-  doneHomework(userId,doneList){
-    return this.post("doneHomework",{userId,doneList});
-  }
-};
+async function post(data) {
+  const res = await fetch(GAS_URL, {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
+  return res.json();
+}
 
+async function getUserId() {
+  const profile = await liff.getProfile();
+  return profile.userId;
+}
