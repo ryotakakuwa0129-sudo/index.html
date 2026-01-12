a7bef6648c9ca6c916724bb64d06cf342dee3dbb
ryotakakuwa0129-sudo <ryotakakuwa0129@gmail.com>
2026-01-12 14:53:02 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const GAS_URL = "GAS_URL";
+const GAS_URL = "https://script.google.com/macros/s/AKfycby0tjXYVUWyPRwqs7r7PwJrrslfTCdZIeQmFwwT1JUfMF9N4a6XwXtgvMz-JDIzIt_mxQ/exec";
 
 async function post(data) {
   const res = await fetch(GAS_URL, {
